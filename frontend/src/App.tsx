d8fcd5e44317767870d46d72f3eb2268d1183e55
L1Pendz <2007rlee@gmail.com>
2026-09-24 11:46:12 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,45 @@
-const App = () => {
+import { ServiceProvider } from "./context/ServiceContext";
+import { AuthContext, AuthProvider } from "./context/AuthContext";
+import { useContext } from "react";
+import ServiceForm from "./components/ServiceForm";
+import { AuthForm } from "./components/AuthForm";
+import ServiceList from "./components/ServiceList";
+
+const MainApp = () => {
+  const authContext = useContext(AuthContext);
+
+  const handleLogout = () => {
+    authContext?.dispatch({ type: "LOGOUT" });
+  };
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="flex items-center justify-between">
+        <h1>The Service Hub</h1>
+        {authContext?.state.isAuthenticated && (
+          <button className="bg-[#333]" onClick={handleLogout}>
+            Sign Out
+          </button>
+        )}
+      </div>
 
-export default App
+      {authContext?.state.isAuthenticated ? <ServiceForm /> : <AuthForm />}
+
+      <hr className="mx-0 border-[#eee]" />
+      <h2>Menu</h2>
+      <ServiceList />
+    </div>
+  );
+};
+
+
+const App = () => {
+  return (
+    <ServiceProvider>
+      <AuthProvider>
+        <MainApp />
+      </AuthProvider>
+    </ServiceProvider>
+  );
+};
+
+export default App;
